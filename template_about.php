5777f7a16be44dde7b674f895ab46fc187b8f0f7
unknown <lucie.g@codeur.online>
2018-01-17 15:01:54 +0100

    page About terminée sauf icônes branding

--- template_about.php
+++ template_about.php
@@ -81,80 +81,81 @@ endif; ?>
 	</section>
 	
 	
+	<!-------------------------SECTION CLASSICO SERVICE------------------------------------->
 	
 	<section id="classico_services" class="container-fluid">
 		<div class="container">
 			<div class="row">
+			
 				<div class="col-10 mr-auto ml-auto text-center">
-					<p class="grey_title">Lorem ipsum dolor sit amet</p>
-					<h2>Classico Services</h2>
-					<img src="assets/img/separator.svg" alt="separator design" width="35" height="2"/>
-					<p class="classico_paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris .</p>
+					<p class="grey_title"><?php the_field('classico_sub'); ?></p>
+					<h2><?php the_field('classico_title'); ?></h2>
+					<img src='<?php the_field('classico_img'); ?>' alt ="separator design" width="35" height="2"/>
+					<p class="classico_paragraph"><?php the_field('classico_paragraph'); ?></p>
 				</div>
+				
 			</div>
+			
+			
+			<!--BOUCLE BRANDING-->
+			
 			<div class="row">
+			
+			
+			<?php $paged = (get_query_var('branding')) ? get_query_var('branding') : 1;
+            $args = array(
+            'posts_per_page' => 6,
+            'post_type' => 'branding',
+            'paged' => $paged );
+            query_posts($args); 
+
+            /*query_posts('post_type=post'); */ 
+            if(have_posts()) :
+            while(have_posts()) : the_post(); ?>
+
+			
 				<div class="ml-auto col-3 col-sm-2 col-md-2 col-lg-1">
-					<i class="fa fa-headphones fa-2x" aria-hidden="true"></i>
-				</div>
-				<div class="col-8 col-sm-auto col-md-4 col-lg-3">
-					<p class="branding">Branding</p>
-					<p class="branding_paragraph">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur</p>
+                    <i class="fa fa-2x <?php the_field('icone_branding'); ?>"></i>
 				</div>
 				
-				<div class="ml-auto col-3 col-sm-2 col-md-2 col-lg-1">
-					<i class="fa fa-headphones fa-2x" aria-hidden="true"></i>
-				</div>
 				<div class="col-8 col-sm-auto col-md-4 col-lg-3">
-					<p class="branding">Branding</p>
-					<p class="branding_paragraph">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur</p>
+					<p class="branding"><?php the_field('titre_branding'); ?></p>
+					<p class="branding_paragraph"><?php the_field('paragraph_branding'); ?></p>
 				</div>
 				
-				<div class="ml-auto col-3 col-sm-2 col-md-2 col-lg-1">
-					<i class="fa fa-headphones fa-2x" aria-hidden="true"></i>
-				</div>
-				<div class="col-8 col-sm-auto col-md-4 col-lg-3">
-					<p class="branding">Branding</p>
-					<p class="branding_paragraph">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur</p>
-				</div>
+			
+                    <?php 
+
+                                    endwhile; 
+
+                                    else :
+                                        echo'<p>Pas de contenu trouvé</p>';
+
+                                    endif; ?>
+
+                    <?php wp_reset_query(); ?>	
 				
-				<div class="ml-auto col-3 col-sm-2 col-md-2 col-lg-1">
-					<i class="fa fa-headphones fa-2x" aria-hidden="true"></i>
-				</div>
-				<div class="col-8 col-sm-auto col-md-4 col-lg-3">
-					<p class="branding">Branding</p>
-					<p class="branding_paragraph">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur</p>
-				</div>
-				
-				<div class="ml-auto col-3 col-sm-2 col-md-2 col-lg-1">
-					<i class="fa fa-headphones fa-2x" aria-hidden="true"></i>
-				</div>
-				<div class="col-8 col-sm-auto col-md-4 col-lg-3">
-					<p class="branding">Branding</p>
-					<p class="branding_paragraph">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur</p>
-				</div>
-				
-				<div class="ml-auto col-3 col-sm-2 col-md-2 col-lg-1">
-					<i class="fa fa-headphones fa-2x" aria-hidden="true"></i>
-				</div>
-				<div class="col-8 col-sm-auto col-md-4 col-lg-3">
-					<p class="branding">Branding</p>
-					<p class="branding_paragraph1">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur</p>
-				</div>
-				
+                
 			</div>
 		</div>
 	</section>
+	
+	<!------------------------------BLANCO FEATURES------------------------------------------->
+	
 	<section id="blanco" class="container-fluid">
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-10">
-					<p class="grey_title">Lorem ipsum dolor sit amet</p>
-					<h2>Check more Blanco features</h2>
-					<img src="assets/img/separator.svg" alt="separator design" width="35" height="2"/>
-					<p class="blanco_paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore<br/>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris .</p>
+				
+					<p class="grey_title"><?php the_field('ss_titre'); ?></p>
+					<h2><?php the_field('titre_blanco'); ?></h2>
+					<img src='<?php the_field('image_blanco'); ?>' alt="separator design" width="35" height="50"/>
+					<p class="blanco_paragraph"><?php the_field('blanco_paragraph'); ?></p>
+					
 				</div>
+				
 				<div class="col-xs-12 col-sm-12 col-md-2 text-center align-self-center">
-					<button>More Details</button>
+					<button type="button" href="<?php the_field('home_link'); ?>" target="_blank">DETAILS</button>
 				</div>
 				
 			</div>
@@ -163,11 +164,3 @@ endif; ?>
 </main>
 
 <?php get_footer(); ?>
-
-
-
-
-
-
-
-
